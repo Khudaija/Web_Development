47763a7082ac24a24bb3d2eb11e0636ffb2ba85b
Khudaija Pinjari <86610468+Khudaija@users.noreply.github.com>
2024-03-02 23:43:31 +0530

    initial commit

---
+++ 3_JS_Project/1_practice_set/1_problem.js
@@ -0,0 +1,10 @@
+// Create a const object called "product to store information shown in the picture"
+
+const product= {
+    title:"Ball pen",
+    rating: 4,
+    offer:5,
+    price: 270,
+};
+
+console.log(product);
---
+++ 3_JS_Project/1_practice_set/2_problem.js
@@ -0,0 +1,10 @@
+// Create a const object called "profile" to store information shown in the picture
+
+const profile ={
+    username: "@khudaija",
+    inFollow: false,
+    followers: 123,
+    folowing: 123,
+};
+
+console.log(profile);
