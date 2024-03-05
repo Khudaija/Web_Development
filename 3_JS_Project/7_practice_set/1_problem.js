62e4b0bb49a339ad80aa73ca39db2cf853890c9e
Khudaija Pinjari <86610468+Khudaija@users.noreply.github.com>
2024-03-05 23:16:43 +0530

    initial commit

---
+++ 3_JS_Project/7_practice_set/1_problem.js
@@ -0,0 +1,7 @@
+let newBtn = document.createElement("button");
+newBtn.innerText = "Click me!";
+
+newBtn.style.color = "white";
+newBtn.style.backgroundColor = "red";
+
+document.querySelector("body").prepend(newBtn);
---
+++ 3_JS_Project/7_practice_set/2_problem.js
@@ -0,0 +1,7 @@
+let para = document.querySelector("p");
+// para.getAttribute("class");
+// para.setAttribute("class", "newClass");
+
+// para.classList;
+para.classList.add("newClass");
+// para.classList.remove("content"); //remove class
