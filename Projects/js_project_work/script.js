a0387cd60438989b773a64f446ec199ddfbc2911
Khudaija Pinjari <86610468+Khudaija@users.noreply.github.com>
2024-03-07 00:39:07 +0530

    initial commit

---
+++ Projects/js_project_work/script.js
@@ -0,0 +1,5 @@
+function showInput() {
+    document.getElementById('display').innerHTML = document.getElementById("name").value;
+
+    document.getElementById('show').innerHTML = document.getElementById("adress").value;
+}
